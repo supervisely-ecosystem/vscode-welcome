c90544d577cf735992a41eca64795e854eb33e86
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-13 13:19:06 +0200

    clean

--- src/main.py
+++ src/main.py
@@ -43,30 +43,6 @@ app.include_router(card_general.router)
 templates = sly.app.fastapi.Jinja2Templates(directory="templates")
 sly.app.fastapi.enable_hot_reload_on_debug(app, templates)
 
-# gettrace = getattr(sys, "gettrace", None)
-# if gettrace is None:
-#     print("Can not detect debug mode, no sys.gettrace")
-# elif gettrace() and templates is not None:
-#     print("In debug mode ...")
-# import arel
-
-# hot_reload = arel.HotReload(paths=[arel.Path(".")])
-# app.add_websocket_route("/hot-reload", route=hot_reload, name="hot-reload")
-# app.add_event_handler("startup", hot_reload.startup)
-# app.add_event_handler("shutdown", hot_reload.shutdown)
-# templates.env.globals["DEBUG"] = "1"
-# templates.env.globals["hot_reload"] = hot_reload
-# # else:
-# #     print("In runtime mode ...")
-
-
-# hot_reload = arel.HotReload(paths=[arel.Path(".")])
-# app.add_websocket_route("/hot-reload", route=hot_reload, name="hot-reload")
-# app.add_event_handler("startup", hot_reload.startup)
-# app.add_event_handler("shutdown", hot_reload.shutdown)
-# templates.env.globals["DEBUG"] = "1"
-# templates.env.globals["hot_reload"] = hot_reload
-
 
 @app.get("/")
 async def read_index(request: Request):
