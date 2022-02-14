41ae159346162c7bfc5376ee5f941165d8335d5a
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-14 11:35:13 +0200

    radio table - done

--- src/card_app_template.py
+++ src/card_app_template.py
@@ -1,13 +1,31 @@
 import supervisely as sly
 
+
+def format_github_column(value):
+    new_val = f"""
+    <i class="zmdi zmdi-github mr5"></i>
+    <a href="{value}" target="_blank">{value}</a>
+"""
+    return new_val
+
+
 templates_table = sly.app.widgets.RadioTable(
     widget_id="appTemplates",
     columns=["name", "github", "column #3"],
     rows=[
-        ["Hello <user>!", "gh1", "-"],
-        ["Random point", "gh2", "-"],
+        [
+            "Hello <user>!",
+            "https://github.com/supervisely-ecosystem/dev-names-generator.git",
+            None,
+        ],
+        [
+            "Random point",
+            "https://github.com/supervisely-ecosystem/dev-names-generator.git",
+            None,
+        ],
     ],
     subtitles={"name": "subname"},
+    column_formatters={"github": format_github_column},
 )
 
 
