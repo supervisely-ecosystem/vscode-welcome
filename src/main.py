b4dd564803b1532f9ca1f4dc262a8357859d52d6
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-15 09:12:25 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -16,14 +16,10 @@ print(f"App root directory: {app_repo_dir}")
 
 # @TODO: repo visibility
 # @TODO: start new vs open recent
-# @TODO: replace special symbols (space-> '-') in app-name
-# @TODO: debug mode -add x-debug-mode : 1 in header
 
 # order matters
 load_dotenv(os.path.join(app_repo_dir, "secret.env"))
 load_dotenv(os.path.join(app_repo_dir, "debug.env"))
-
-# import src.preview_paths as step1
 
 import card_name
 import card_example
@@ -31,25 +27,20 @@ import card_example
 # init global state and data (singletons)
 last_state = sly.app.LastStateJson({"activeStep": 1})
 data = sly.app.DataJson({})
-card_name.init(data, last_state)
-card_example.init(data, last_state)
+card_name.init(app, data, last_state, templates)
+card_example.init(data, last_state, templates)
 
 
 app = FastAPI()
+templates = sly.app.fastapi.Jinja2Templates(directory="templates")
 sly_app = sly.app.fastapi.create()
 app.mount("/sly", sly_app)
-app.include_router(card_name.router)
+
 # app.mount("/static", StaticFiles(directory="static", html=True), name="static")
-templates = sly.app.fastapi.Jinja2Templates(directory="templates")
+
 sly.app.fastapi.enable_hot_reload_on_debug(app, templates)
 
 
 @app.get("/")
 async def read_index(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
-            **card_example.get_jinja2_context(),
-        },
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
