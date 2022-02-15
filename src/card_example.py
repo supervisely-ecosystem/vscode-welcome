c9f52ae8dfdeaf4d8c26f920f1ac13fae391fce5
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-15 09:46:49 +0200

    table with description

--- src/card_example.py
+++ src/card_example.py
@@ -6,28 +6,40 @@ from supervisely.app.fastapi import Jinja2Templates
 def format_github_column(value):
     new_val = f"""
     <i class="zmdi zmdi-github mr5"></i>
-    <a href="{value}" target="_blank">{value}</a>
-"""
+    <a href="{value}" target="_blank">github</a>
+    """
+    return new_val
+
+
+def format_description_column(value):
+    new_val = f"""
+    <div style="text-align: left">{value}</div>
+    """
     return new_val
 
 
 examples = sly.app.widgets.RadioTable(
     widget_id="examples",
-    columns=["name", "github", "column #3"],
+    columns=["name", "description", "github", "column #3"],
     rows=[
         [
             "Hello <user>!",
+            "How to handle button clicks and dynamically update variable",
             "https://github.com/supervisely-ecosystem/dev-names-generator.git",
             None,
         ],
         [
-            "Random point",
+            "Random circle",
+            "Upload local image, modify it and preview both images",
             "https://github.com/supervisely-ecosystem/dev-names-generator.git",
             "abcd",
         ],
     ],
     subtitles={"name": "subname"},
-    column_formatters={"github": format_github_column},
+    column_formatters={
+        "github": format_github_column,
+        "description": format_description_column,
+    },
 )
 
 
